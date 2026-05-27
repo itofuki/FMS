04e1588febc2e5d6dc95ebc7ef5b4ead9fa0b5c9
itofuki <itofuki.it@gmail.com>
2026-05-27 10:28:05 +0900

    change theme color from white to black

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
         name: 'FMS',
         short_name: 'FMS',
         description: 'FMSアプリケーション',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
+        theme_color: '#000000',
+        background_color: '#000000',
         display: 'standalone',
         icons: [
           {
